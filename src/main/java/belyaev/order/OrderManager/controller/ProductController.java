4da4f312bf89feb69dfa3605dbe363bbd967ee6a
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2023-07-02 19:37:24 +0300

    Add list of products in index.html.

--- src/main/java/belyaev/order/OrderManager/controller/ProductController.java
+++ src/main/java/belyaev/order/OrderManager/controller/ProductController.java
@@ -6,17 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ProductController {
+    List<Product> products = new ArrayList<>();
     @GetMapping("/")
     public String greeting(Model model) {
-        model.addAttribute("product", new Product());
+        model.addAttribute("product_input", new Product());
+        model.addAttribute("product_list", products);
         return "index";
     }
 
     @PostMapping("/add_product")
     public String addProduct(Product product, Model model) {
         System.out.println(product.getProductName() + " is here!");
+        products.add(product);
         return "redirect:/";
     }
 }
