a2016697ea79ec88f67ebdc0321ced0696414b1c
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2023-07-21 15:41:26 +0300

    Add UserRepository and RoleRepository.

---
+++ src/main/java/belyaev/order/OrderManager/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package belyaev.order.OrderManager.repository;
+
+import belyaev.order.OrderManager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String name);
+}
